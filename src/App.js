daa4a59306ba085a8dcb2477862c18026882fe2a
Tushar Debnath <txdebnath@gmail.com>
2023-05-22 12:12:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider, Button, Box, Image } from '@revolut/ui-kit'
+
+
 
 function App() {
 
   const onCancel = () => {
-    window.parent.postMessage({ pluginMessage: { type: "cancel" },pluginId:"1241379539591448233" }, "*");
+    window.parent.postMessage({ pluginMessage: { type: "cancel" }, pluginId: "1241379539591448233" }, "*");
   };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={onCancel}>Cancel</button>
+        <Image
+          src='https://assets.revolut.com/assets/3d-images/3D070.png'
+          alt='Checklist'
+          size={200}
+        />
+        <Provider mode='auto' highContrast='auto'>
+          <Box p='s-16'>
+            <Button onClick={onCancel} elevated>Close Window</Button>
+          </Box>
+        </Provider>
       </header>
-      
+
     </div>
   );
 }
